eaaab888eca62078eb06225b509c5636f48a3690
GuanYu Lin <lgy3030@gmail.com>
2019-02-26 21:25:02 +0800

    Update

--- project/main/views.py
+++ project/main/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Jimmy',
+        'title': 'first',
+        'content': 'first content',
+        'date': 'May 23,2018',
+    },
+    {
+        'author': 'Amy',
+        'title': 'second',
+        'content': 'second content',
+        'date': 'May 24,2018',
+    }
+]
+
 
 def home(request):
-    return render(request, 'main/home.html')
+    context = {'posts': posts}
+    return render(request, 'main/home.html', context)
 
 
 def about(request):
-    return render(request, 'main/about.html')
+    return render(request, 'main/about.html',{'title':'about'})
