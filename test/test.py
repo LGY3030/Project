15bb12aa817812a787bcc4db00cfd46732e65283
GuanYu Lin <lgy3030@gmail.com>
2019-03-10 18:27:24 +0800

    Update

--- test/test.py
+++ test/test.py
@@ -1,14 +1,14 @@
 
 # coding: utf-8
 
-# In[6]:
+# In[16]:
 
 
 import pandas as pd
 import numpy as np
 import keras
 from keras.models import Sequential
-from keras.layers import Dense,LSTM,Dropout,Activation
+from keras.layers import Dense,LSTM,Dropout,Activation,TimeDistributed
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 from sklearn.utils import shuffle
@@ -16,7 +16,7 @@ from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
 
-# In[12]:
+# In[52]:
 
 
 def readData():
@@ -55,70 +55,330 @@ def splitData(X,Y,Z,rate):
     Z_val = Z[:int(Z.shape[0]*rate)]
     return X_train, Y_train, X_val, Y_val,Z_val
 
-def buildModel(train_x,train_y):
+def buildModel_1(train_x,train_y,bs):
     model = Sequential()
-    model.add(LSTM(200, input_length=train_x.shape[1],input_dim= train_x.shape[2],return_sequences=True))
-    model.add(Dropout(0.6))
-    model.add(LSTM(150))
-    model.add(Dropout(0.6))
+    model.add(LSTM(10, input_length=train_x.shape[1],input_dim= train_x.shape[2]))
     model.add(Dense(1))
-    model.add(Activation('linear'))
     model.compile(loss='mean_squared_error', optimizer='adam')
     model.summary()
     callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
-    model.fit(train_x,train_y, epochs=300, batch_size=32, validation_split=0.1, callbacks=[callback])
-    return model
-def predict(model,val_x,val_y,val_z):
+    model.fit(train_x,train_y, epochs=10, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[10,0,0,0,0]
+def buildModel_2(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(10, input_length=train_x.shape[1],input_dim= train_x.shape[2],return_sequences=True))
+    model.add(LSTM(10))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=10, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[10,10,0,0,0]
+def buildModel_3(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(10, input_length=train_x.shape[1],input_dim= train_x.shape[2],return_sequences=True))
+    model.add(LSTM(10,return_sequences=True))
+    model.add(LSTM(10))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=1000, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[10,10,10,0,0]
+def buildModel_4(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(10, input_length=train_x.shape[1],input_dim= train_x.shape[2],return_sequences=True))
+    model.add(LSTM(10,return_sequences=True))
+    model.add(LSTM(10,return_sequences=True))
+    model.add(LSTM(10))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=1000, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[10,10,10,10,0]
+def buildModel_5(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(10, input_length=train_x.shape[1],input_dim= train_x.shape[2],return_sequences=True))
+    model.add(LSTM(10,return_sequences=True))
+    model.add(LSTM(10,return_sequences=True))
+    model.add(LSTM(10,return_sequences=True))
+    model.add(LSTM(10))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=1000, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[10,10,10,10,10]
+def buildModel_6(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(32, input_length=train_x.shape[1],input_dim= train_x.shape[2]))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=1000, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[32,0,0,0,0]
+def buildModel_7(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(32, input_length=train_x.shape[1],input_dim= train_x.shape[2],return_sequences=True))
+    model.add(LSTM(32))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=1000, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[32,32,0,0,0]
+def buildModel_8(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(32, input_length=train_x.shape[1],input_dim= train_x.shape[2],return_sequences=True))
+    model.add(LSTM(32,return_sequences=True))
+    model.add(LSTM(32))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=1000, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[32,32,32,0,0]
+def buildModel_9(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(32, input_length=train_x.shape[1],input_dim= train_x.shape[2],return_sequences=True))
+    model.add(LSTM(32,return_sequences=True))
+    model.add(LSTM(32,return_sequences=True))
+    model.add(LSTM(32))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=1000, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[32,32,32,32,0]
+def buildModel_10(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(32, input_length=train_x.shape[1],input_dim= train_x.shape[2],return_sequences=True))
+    model.add(LSTM(32,return_sequences=True))
+    model.add(LSTM(32,return_sequences=True))
+    model.add(LSTM(32,return_sequences=True))
+    model.add(LSTM(32))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=1000, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[32,32,32,32,32]
+def buildModel_11(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(64, input_length=train_x.shape[1],input_dim= train_x.shape[2]))
+    model.add(Dropout(0.3))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=1000, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[64,0,0,0,0]
+def buildModel_12(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(64, input_length=train_x.shape[1],input_dim= train_x.shape[2],return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(64))
+    model.add(Dropout(0.3))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=1000, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[64,64,0,0,0]
+def buildModel_13(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(64, input_length=train_x.shape[1],input_dim= train_x.shape[2],return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(64,return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(64))
+    model.add(Dropout(0.3))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=1000, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[64,64,64,0,0]
+def buildModel_14(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(64, input_length=train_x.shape[1],input_dim= train_x.shape[2],return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(64,return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(64,return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(64))
+    model.add(Dropout(0.3))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=1000, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[64,64,64,64,0]
+def buildModel_15(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(64, input_length=train_x.shape[1],input_dim= train_x.shape[2],return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(64,return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(64,return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(64,return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(64))
+    model.add(Dropout(0.3))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=1000, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[64,64,64,64,64]
+def buildModel_16(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(128, input_length=train_x.shape[1],input_dim= train_x.shape[2],return_sequences=True))
+    model.add(Dropout(0.45))
+    model.add(LSTM(128))
+    model.add(Dropout(0.45))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=1000, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[128,128,0,0,0]
+def buildModel_17(train_x,train_y,bs):
+    model = Sequential()
+    model.add(LSTM(128, input_length=train_x.shape[1],input_dim= train_x.shape[2],return_sequences=True))
+    model.add(Dropout(0.45))
+    model.add(LSTM(128,return_sequences=True))
+    model.add(Dropout(0.45))
+    model.add(LSTM(128))
+    model.add(Dropout(0.45))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.summary()
+    callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
+    model.fit(train_x,train_y, epochs=1000, batch_size=bs, validation_split=0.1, callbacks=[callback])
+    return model,[128,128,128,0,0]
+def predict(model,layer,val_x,val_y,val_z,x,y):
     a=range(0,val_y.shape[0])
     val_y=val_y.reshape(-1)
     val_z=val_z.reshape(-1)
     plt.plot(a,val_y)
     b=[]
     co=0
-    coo=0
     for i in range(0,val_x.shape[0]):
         temp=val_x[i]
         temp=temp.reshape(1,1,9)
-        z=model.predict(temp, verbose=0)
+        z=int(model.predict(temp, verbose=0))
         if val_y[i]>=val_z[i] and z>=val_z[i]:
             co=co+1
         if val_y[i]<val_z[i] and z<val_z[i]:
             co=co+1
-        sub=val_y[i]-val_z[i]
-        sub_a=0.5*sub+val_z[i]
-        sub_b=1.5*sub+val_z[i]
-        sub2=val_z[i]-val_y[i]
-        sub2_a=val_z[i]-0.5*sub2
-        sub2_b=val_z[i]-1.5*sub2
-        if val_y[i]>=val_z[i] and z>=val_z[i]:
-            if z>=sub_a and z<=sub_b:
-                coo=coo+1
-        if val_y[i]<val_z[i] and z<val_z[i]:
-            if z<=sub2_a and z>=sub2_b:
-                coo=coo+1
         b.append(z)
     b=np.array(b)
     b=b.reshape(-1)
     plt.plot(a,b)
     acc=100*(co/val_x.shape[0])
     print("accuracy:"+str(acc)+"%")
-    acc2=100*(coo/val_x.shape[0])
-    print("accuracy(50%):"+str(acc2)+"%")
-    plt.show()
+    plt.savefig('img/'+str(layer[0])+'+'+str(layer[1])+'+'+str(layer[2])+'+'+str(layer[3])+'+'+str(layer[4])+'+'+'ac,'+str(acc)+"%"+'+'+'lb,'+str(x)+'+'+'bs,'+str(y)+'.jpg')
+    return str(acc)
 
 
-# In[13]:
+# In[53]:
 
 
-train=readData()
-train=dropData(train)
-temp=train
-train=sta(train)
-train_x1,train_y1,train_z1=buildTrain(train)
-train_x2,train_y2,train_z2=buildTrain(temp)
-train_x,train_y,train_z=train_x1,train_y2,train_z2
-train_x,train_y,train_z= shuffle(train_x,train_y,train_z)
-train_x,train_y, val_x, val_y ,val_z= splitData(train_x,train_y,train_z, 0.05)
-model=buildModel(train_x,train_y)
-predict(model,val_x,val_y,val_z)
+lookback=[1,3,5,7,14,21,30,60,90,120,150,180,210,240,270,300,330,360]
+batch_size=[4,8,16,32,64,128,256]
+col_name=["1","2","3","4","5","acc","lookback","batch_size"]
+df=pd.DataFrame(columns=col_name)
+for i in lookback:
+    for j in batch_size:
+        data=[]
+        train=readData()
+        train=dropData(train)
+        temp=train
+        train=sta(train)
+        train_x1,train_y1,train_z1=buildTrain(train,i,1)
+        train_x2,train_y2,train_z2=buildTrain(temp,i,1)
+        train_x,train_y,train_z=train_x1,train_y2,train_z2
+        train_x,train_y,train_z= shuffle(train_x,train_y,train_z)
+        train_x,train_y, val_x, val_y ,val_z= splitData(train_x,train_y,train_z, 0.05)
+        model_1,layer_1=buildModel_1(train_x,train_y,j)
+        model_2,layer_2=buildModel_2(train_x,train_y,j)
+        model_3,layer_3=buildModel_3(train_x,train_y,j)
+        model_4,layer_4=buildModel_4(train_x,train_y,j)
+        model_5,layer_5=buildModel_5(train_x,train_y,j)
+        model_6,layer_6=buildModel_6(train_x,train_y,j)
+        model_7,layer_7=buildModel_7(train_x,train_y,j)
+        model_8,layer_8=buildModel_8(train_x,train_y,j)
+        model_9,layer_9=buildModel_9(train_x,train_y,j)
+        model_10,layer_10=buildModel_10(train_x,train_y,j)
+        model_11,layer_11=buildModel_11(train_x,train_y,j)
+        model_12,layer_12=buildModel_12(train_x,train_y,j)
+        model_13,layer_13=buildModel_13(train_x,train_y,j)
+        model_14,layer_14=buildModel_14(train_x,train_y,j)
+        model_15,layer_15=buildModel_15(train_x,train_y,j)
+        model_16,layer_16=buildModel_16(train_x,train_y,j)
+        model_17,layer_17=buildModel_17(train_x,train_y,j)
+        pre=predict(model_1,layer_1,val_x,val_y,val_z,i,j)
+        layer=layer_1
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        pre=predict(model_2,layer_2,val_x,val_y,val_z,i,j)
+        layer=layer_2
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        predict(model_3,layer_3,val_x,val_y,val_z,i,j)
+        layer=layer_3
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        predict(model_4,layer_4,val_x,val_y,val_z,i,j)
+        layer=layer_4
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        predict(model_5,layer_5,val_x,val_y,val_z,i,j)
+        layer=layer_5
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        predict(model_6,layer_6,val_x,val_y,val_z,i,j)
+        layer=layer_6
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        predict(model_7,layer_7,val_x,val_y,val_z,i,j)
+        layer=layer_7
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        predict(model_8,layer_8,val_x,val_y,val_z,i,j)
+        layer=layer_8
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        predict(model_9,layer_9,val_x,val_y,val_z,i,j)
+        layer=layer_9
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        predict(model_10,layer_10,val_x,val_y,val_z,i,j)
+        layer=layer_10
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        predict(model_11,layer_11,val_x,val_y,val_z,i,j)
+        layer=layer_11
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        predict(model_12,layer_12,val_x,val_y,val_z,i,j)
+        layer=layer_12
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        predict(model_13,layer_13,val_x,val_y,val_z,i,j)
+        layer=layer_13
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        predict(model_14,layer_14,val_x,val_y,val_z,i,j)
+        layer=layer_14
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        predict(model_15,layer_15,val_x,val_y,val_z,i,j)
+        layer=layer_15
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        predict(model_16,layer_16,val_x,val_y,val_z,i,j)
+        layer=layer_16
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        predict(model_17,layer_17,val_x,val_y,val_z,i,j)
+        layer=layer_17
+        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":i,"batch_size":j})
+        df=pd.concat([pd.DataFrame(data), df], ignore_index=True,sort=True)
+df.to_csv('data.csv', encoding='utf_8_sig')
+
+
+# In[15]:
+
+
+predict(model_3,val_x,val_y,val_z)
 
