c38a76913cf18dd5f28eb21d46f1953ea7c0c222
GuanYu Lin <lgy3030@gmail.com>
2019-04-17 23:15:37 +0800

    Update

--- test/train_crop&weather&oil.py
+++ test/train_crop&weather&oil.py
@@ -69,7 +69,9 @@ def predict(model,layer,val_x,val_y,val_z,x,y,name,num):
     val_z=val_z.reshape(-1)
     plt.plot(a,val_y)
     b=[]
+    c=[]
     co=0
+    totalss = 0
     for i in range(0,val_x.shape[0]):
         temp=val_x[i]
         temp=temp.reshape(1,x,27)
@@ -78,39 +80,48 @@ def predict(model,layer,val_x,val_y,val_z,x,y,name,num):
             co=co+1
         if val_y[i]<val_z[i] and z<val_z[i]:
             co=co+1
+        if val_y[i] >= z:
+            sub = val_y[i]l - z
+        if val_y[i] < z:
+            sub = z - val_y[i]
         b.append(z)
+        c.append(sub)
     b=np.array(b)
     b=b.reshape(-1)
     plt.plot(a,b)
     acc=100*(co/val_x.shape[0])
+    for i in c:
+        totalss = totalss + i * i
+    StandardDeviation=float((totalss / len(c))**0.5)
+    AverageDeviation=sum(c)/len(c)
     plt.savefig('img/'+name+'('+str(num)+')'+str(layer[0])+'+'+str(layer[1])+'+'+str(layer[2])+'+'+str(layer[3])+'+'+str(layer[4])+'+'+'ac,'+str(acc)+"%"+'+'+'lb,'+str(x)+'+'+'bs,'+str(y)+'.jpg')
     plt.clf()
-    return str(acc)
+    return str(acc),StandardDeviation,AverageDeviation
 
 
 # In[ ]:
 
 
-lookback=7
+lookbacks=[30,60,90,120]
 batch_size=32
-col_name=["1","2","3","4","5","acc","lookback","batch_size","name","num"]
+col_name=["1","2","3","4","5","acc","lookback","batch_size","name","StandardDeviation","AverageDeviation","num"]
 place_name=["train+oil+weather+國定假日+拜拜日 雲林","train+oil+weather+國定假日+拜拜日 嘉義","train+oil+weather+國定假日+拜拜日 彰化","train+oil+weather+國定假日+拜拜日 台南","train+oil+weather+國定假日+拜拜日 高雄","train+oil+weather+國定假日+拜拜日 屏東","train+oil+weather+國定假日+拜拜日 台中","train+oil+weather+國定假日+拜拜日 苗栗","train+oil+weather+國定假日+拜拜日 桃園","train+oil+weather+國定假日+拜拜日 台北"]
-
-for i in place_name:
-    df=pd.DataFrame(columns=col_name)
-    data=[]
-    for j in range(30):
-        train=readData(i)
-        temp=train
-        train=sta(train)
-        train_x1,train_y1,train_z1=buildTrain(train,lookback,1)
-        train_x2,train_y2,train_z2=buildTrain(temp,lookback,1)
-        train_x,train_y,train_z=train_x1,train_y2,train_z2
-        train_x,train_y,train_z= shuffle(train_x,train_y,train_z)
-        train_x,train_y, val_x, val_y ,val_z= splitData(train_x,train_y,train_z, 0.05)
-        model,layer=buildModel(train_x,train_y,batch_size)
-        pre=predict(model,layer,val_x,val_y,val_z,lookback,batch_size,i,j)
-        data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":lookback,"batch_size":batch_size,"name":i,"num":j})
-    df=pd.concat([pd.DataFrame(data), df], ignore_index=True,sort=True)
-    df.to_csv(i+'_data'+'(lookback  '+str(lookback)+')'+'(bs  '+str(batch_size)+')'+'.csv', encoding='utf_8_sig')
+for lookback in lookbacks:
+    for i in place_name:
+        df=pd.DataFrame(columns=col_name)
+        data=[]
+        for j in range(30):
+            train=readData(i)
+            temp=train
+            train=sta(train)
+            train_x1,train_y1,train_z1=buildTrain(train,lookback,1)
+            train_x2,train_y2,train_z2=buildTrain(temp,lookback,1)
+            train_x,train_y,train_z=train_x1,train_y2,train_z2
+            train_x,train_y,train_z= shuffle(train_x,train_y,train_z)
+            train_x,train_y, val_x, val_y ,val_z= splitData(train_x,train_y,train_z, 0.05)
+            model,layer=buildModel(train_x,train_y,batch_size)
+            pre,StandardDeviation,AverageDeviation=predict(model,layer,val_x,val_y,val_z,lookback,batch_size,i,j)
+            data.append({"1":layer[0],"2":layer[1],"3":layer[2],"4":layer[3],"5":layer[4],"acc":pre,"lookback":lookback,"batch_size":batch_size,"name":i,"StandardDeviation":StandardDeviation,"AverageDeviation":AverageDeviation,"num":j})
+        df=pd.concat([pd.DataFrame(data), df], ignore_index=True,sort=True)
+        df.to_csv(i+'_data'+'(lookback  '+str(lookback)+')'+'(bs  '+str(batch_size)+')'+'.csv', encoding='utf_8_sig')
 
